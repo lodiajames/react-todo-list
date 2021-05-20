65518555d75918136381c0c12de8e03dce37bffa
Ibrahima diallo <lodiajames@hotmail.com>
2021-05-20 16:33:50 -0400

    first commit

---
+++ src/components/TodoInput.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class TodoInput extends Component {
+    render() {
+        return (
+            <h1>
+                hello from todo input
+            </h1>
+        )
+    }
+}
+
